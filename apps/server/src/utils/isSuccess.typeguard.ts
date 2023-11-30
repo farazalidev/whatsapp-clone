0b28518ad9f02ed72151cfdcbee853e91acaded4
faraz ali <farazalidev@gmail.com>
2023-11-30 13:13:22 +0500

    isSuccess type guard updated

--- apps/server/src/utils/isSuccess.typeguard.ts
+++ apps/server/src/utils/isSuccess.typeguard.ts
@@ -1,7 +1,5 @@
 import { ResponseType } from 'src/Misc/ResponseType.type';
 
-export function isSuccess(
-  response: ResponseType,
-): response is { success: true; successMessage: string } {
-  return response.success === true;
+export function isSuccess(response: ResponseType): response is { success: true; successMessage: string } {
+  return response?.success === true;
 }
