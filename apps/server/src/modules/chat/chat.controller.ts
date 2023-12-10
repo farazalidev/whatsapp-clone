96dc044e4b5d637bb98abdf79870a24a691d781f
faraz ali <farazalidev@gmail.com>
2023-12-10 11:12:23 +0500

    get chat by id

--- apps/server/src/modules/chat/chat.controller.ts
+++ apps/server/src/modules/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException } from '@nestjs/common';
+import { Controller, Get, HttpException, Param } from '@nestjs/common';
 import { GetUser } from '../auth/decorators/getuser.decorator';
 import { LoginPayload } from '../auth/auth.service';
 import { ChatService } from './chat.service';
@@ -14,6 +14,16 @@ export class ChatController {
   async getUserChats(@GetUser() user: LoginPayload): Promise<UserChatEntity[]> {
     const response = await this.chatSer.getUserChats(user.user_id);
     console.log('🚀 ~ file: chat.controller.ts:16 ~ ChatController ~ getUserChats ~ response:', response);
+    if (!isSuccess(response)) {
+      throw new HttpException(response.error.message, response.error.statusCode);
+    }
+    return response.data;
+  }
+
+  // get user_chat by id
+  @Get(':id')
+  async getUserChatById(@Param() param: { id: string }, @GetUser() user: LoginPayload): Promise<UserChatEntity> {
+    const response = await this.chatSer.getChatByIdService(user.user_id, param.id);
     if (!isSuccess(response)) {
       throw new HttpException(response.error.message, response.error.statusCode);
     }
