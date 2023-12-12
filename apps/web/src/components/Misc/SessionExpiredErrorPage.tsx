c8470af5600419eaf5768bd7174fdc679b634c75
faraz ali <farazalidev@gmail.com>
2023-12-12 17:49:59 +0500

    Session Error page text fixed.

--- apps/web/src/components/Misc/SessionExpiredErrorPage.tsx
+++ apps/web/src/components/Misc/SessionExpiredErrorPage.tsx
@@ -11,14 +11,16 @@ const SessionExpiredErrorPage = () => {
         <div className="flex place-items-center justify-center w-full">
           <Image src={'/icons/session-expired.svg'} width={300} height={300} alt="Error" priority />
         </div>
-        <div className="w-[50%] h-full text-center">
+        <div className="w-[90%] md:w-[30%] line h-full text-center">
           <Typography level={5}>Error 401</Typography>
-          <Typography text_style={'error'}>
+          <Typography text_style={'error'} className="text-justify">
             Oops! It seems like your session has expired. For security reasons, we automatically log users out after a period of inactivity.
             Please log in again to continue enjoying our services. Thank you for your understanding!
           </Typography>
-          <Link href={'/auth/login'} className="mt-4">
-            <Button size={'lg'}>Login Again</Button>
+          <Link href={'/auth/login'}>
+            <Button size={'lg'} className="mt-4">
+              Login Again
+            </Button>
           </Link>
         </div>
       </div>
