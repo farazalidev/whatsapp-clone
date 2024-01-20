70aa2a86e5b642156030b4b01928c6527af81135
farazalidev <farazalidev@gmail.com>
2024-01-20 17:23:21 +0500

    Refactoring Thumbnail.tsx

--- apps/web/src/components/Misc/Thumbnail.tsx
+++ apps/web/src/components/Misc/Thumbnail.tsx
@@ -1,7 +1,8 @@
-import { expectedFileTypes } from '@/global/context/reducers/filesReducer';
-import useColorScheme, { IColorScheme } from '@/hooks/useColorScheme';
+import { useFilesContext } from '@/global/context/filesContext';
+import { FilesActionType, expectedFileTypes } from '@/global/context/reducers/filesReducer';
+import useColorScheme from '@/hooks/useColorScheme';
 import Image from 'next/image';
-import { FC } from 'react';
+import React, { FC, } from 'react';
 
 export interface ThumbnailProps {
   id: string
@@ -15,6 +16,14 @@ export interface ThumbnailProps {
 
 export const Thumbnail: FC<ThumbnailProps> = ({ url, height, width, active, onClick, type, id }) => {
   const colorScheme = useColorScheme()
+  const { dispatch } = useFilesContext()
+
+  const handleRemove = (e: React.MouseEvent, id: string) => {
+    e.stopPropagation()
+    // dispatch({type:FilesActionType.remove_File,payload:{id}})
+    console.log("removing", id);
+
+  }
 
   return (
     <div
@@ -23,18 +32,21 @@ export const Thumbnail: FC<ThumbnailProps> = ({ url, height, width, active, onCl
       className={`group relative flex place-items-center justify-center overflow-hidden border hover:cursor-pointer ${active ? ' border-whatsapp-misc-my_message_bg_dark rounded-lg border-[3px]' : 'border-[2px] border-gray-300  dark:border-gray-600'}`}
     >
       <span className="invisible absolute -bottom-[100%] z-10  h-full w-full overflow-hidden bg-gray-800 bg-opacity-50 group-hover:visible group-hover:top-0" />
+      <span className='absolute top-0 right-0 w-5 h-5 z-20 border' onClick={(e) => handleRemove(e, id)}>
+        {colorScheme === "light" ? <Image src={'/icons/x.svg'} fill alt='remove' /> : <Image src={'/icons/x_white.svg'} fill alt='remove' />}
+      </span>
       {url ? (
         <>
           {type === 'image' ? (
-            <ImageThumbnail url={url} colorScheme={colorScheme} />
+            <ImageThumbnail url={url} />
           ) : type === 'video' ? (
-            <VideoThumbnail url={url} colorScheme={colorScheme} />
+            <VideoThumbnail url={url} />
           ) : (
-            <ImageThumbnail url={url} colorScheme={colorScheme} />
+            <ImageThumbnail url={url} />
           )}
         </>
       ) : (
-        <GenericThumbnail colorScheme={colorScheme} />
+        <GenericThumbnail />
       )}
     </div>
   );
@@ -43,22 +55,21 @@ export const Thumbnail: FC<ThumbnailProps> = ({ url, height, width, active, onCl
 
 interface IImageThumbnail {
   url: string
-  colorScheme: IColorScheme
+
 }
 
-const ImageThumbnail: FC<IImageThumbnail> = ({ url, colorScheme }) => {
+const ImageThumbnail: FC<IImageThumbnail> = ({ url, }) => {
 
   return <span><Image src={url} alt="Thumbnail" fill objectFit="cover" className='relative' />
-    <span className='absolute top-0 right-0 z-20' ><Image src={`${colorScheme === "light" ? '/icons/x.svg' : '/icons/x_white.svg'}`} width={17} height={17} alt='remove' /></span>
   </span>
 }
 
 interface IVideoThumbnail {
   url: string
-  colorScheme: IColorScheme
+
 }
 
-const VideoThumbnail: FC<IVideoThumbnail> = ({ url, colorScheme }) => {
+const VideoThumbnail: FC<IVideoThumbnail> = ({ url, }) => {
 
 
   return <span className="flex place-items-center justify-center">
@@ -67,20 +78,18 @@ const VideoThumbnail: FC<IVideoThumbnail> = ({ url, colorScheme }) => {
       <span className="relative h-5 w-5">
         <Image src={'/icons/play.svg'} alt="play" fill />
       </span>
-      <span className='absolute top-0 right-0 z-20' ><Image src={`${colorScheme === "light" ? '/icons/x.svg' : '/icons/x_white.svg'}`} width={17} height={10} alt='remove' /></span>
     </span>
   </span>
 }
 
 
 interface IGenericThumbnail {
-  colorScheme: IColorScheme
+
 }
 
-const GenericThumbnail: FC<IGenericThumbnail> = ({ colorScheme }) => {
+const GenericThumbnail: FC<IGenericThumbnail> = () => {
 
 
   return <span><Image src={'/icons/preview-generic.svg'} alt="Thumbnail" height={30} width={30} className='relative' />
-    <span className='absolute top-0 right-0 z-20' ><Image src={`${colorScheme === "light" ? '/icons/x.svg' : '/icons/x_white.svg'}`} width={17} height={17} alt='remove' /></span>
   </span>
 }
